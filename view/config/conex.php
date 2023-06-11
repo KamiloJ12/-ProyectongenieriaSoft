4adc8a1f352c7f7190c4490f4fd1831c9b755da7
KamiloJ12 <62122581+KamiloJ12@users.noreply.github.com>
2023-06-11 12:00:12 -0500

    Update conex.php

--- view/config/conex.php
+++ view/config/conex.php
@@ -3,8 +3,9 @@ $hostname_conex = "containers-us-west-49.railway.app";
 $database_conex = "railway";
 $username_conex = "root";
 $password_conex = "IBAOn08BrJ34rSAG0Ybu";
+$port_conex = "6089";
 // creación de la conexión a la base de datos con mysql_connect()
-$conex = mysqli_connect($hostname_conex, $username_conex, $password_conex, $database_conex) or 
+$conex = mysqli_connect($hostname_conex, $username_conex, $password_conex, $database_conex, $port_conex) or 
 die ("No se ha podido conectar al servidor de Base de datos"); 
 
 ?>
