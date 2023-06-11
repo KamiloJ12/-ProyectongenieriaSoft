325f84f753f61a259d02739bd7bc1293505186a1
KamiloJ12 <62122581+KamiloJ12@users.noreply.github.com>
2023-06-11 11:54:12 -0500

    Update conexion.php

--- view/config/conexion.php
+++ view/config/conexion.php
@@ -1,6 +1,6 @@
 <?php
 	
-	$mysqli = new mysqli('localhost', 'root', '', 'proyecto_final');
+	$mysqli = new mysqli('containers-us-west-49.railway.app', 'root', 'IBAOn08BrJ34rSAG0Ybu', 'railway');
 	
 	if($mysqli->connect_error){
 		
