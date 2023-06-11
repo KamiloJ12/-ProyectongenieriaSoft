7aa4cb6a5bb59e471ea223d4518dd4c2c1a6a4fa
KamiloJ12 <62122581+KamiloJ12@users.noreply.github.com>
2023-06-11 14:05:37 -0500

    Update mostrar.php

--- view/horario/mostrar.php
+++ view/horario/mostrar.php
@@ -579,13 +579,15 @@
 
 	<?php
 if(isset($_POST["agregar"])){
-$servername = "localhost";
+
+$servername = "containers-us-west-49.railway.app";
 $username = "root";
-$password = "";
-$dbname = "proyecto_final";
+$password = "IBAOn08BrJ34rSAG0Ybu";
+$dbname = "railway";
 
 // Creamos la conexión
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname, "6089");
+
 
 // Revisamos la conexión
 if ($conn->connect_error) {
